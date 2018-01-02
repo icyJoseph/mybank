68dbf49743e2681b341408f8984b621b56c81872
joseph_ctu <sephxd1234@gmail.com>
2018-01-02 15:42:58 +0100

    refactor: removed white space from test

--- src/actions/balance.test.js
+++ src/actions/balance.test.js
@@ -3,7 +3,6 @@ import * as actions from "./balance";
 
 it("creates an action to set the balance", () => {
   const balance = 0;
-
   const expectedAction = { type: constants.SET_BALANCE, balance };
 
   expect(actions.setBalance(balance)).toEqual(expectedAction);

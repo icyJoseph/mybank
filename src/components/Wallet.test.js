ac73c9d21049a8b8d84cad0637527fce34f493db
joseph_ctu <sephxd1234@gmail.com>
2018-01-02 16:41:32 +0100

    test: Wallet renders correct balance prop

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -3,8 +3,16 @@ import { shallow } from "enzyme";
 import { Wallet } from "./Wallet";
 
 describe("Wallet", () => {
-  const wallet = shallow(<Wallet />);
+  const props = { balance: 20 };
+  const wallet = shallow(<Wallet {...props} />);
+
   it("renders properly", () => {
     expect(wallet).toMatchSnapshot();
+  });
+
+  it("displays the balance from props", () => {
+    expect(wallet.find(".balance").text()).toEqual(
+      `Wallet balance: ${props.balance}`
+    );
   });
 });

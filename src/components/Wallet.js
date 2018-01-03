109a8468ea18ea1d4fa8ee655fcbb8f1dfb81680
joseph_ctu <sephxd1234@gmail.com>
2018-01-03 15:46:58 +0100

    feat: add balance to wallet state

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,13 +2,24 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class Wallet extends Component {
+  state = {
+    balance: undefined
+  };
+
+  updateBalance = e => {
+    this.setState({ balance: parseInt(e.target.value, 10) });
+  };
   render() {
     const { balance } = this.props;
     return (
       <div>
         <h3 className="balance">Wallet balance: {balance}</h3>
         <br />
-        <input type="text" className="input-wallet" />
+        <input
+          type="text"
+          className="input-wallet"
+          onChange={this.updateBalance}
+        />
       </div>
     );
   }

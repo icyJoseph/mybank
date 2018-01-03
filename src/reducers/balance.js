1e25ca021826ddcac3af6e442a092a5c37f40a24
joseph_ctu <sephxd1234@gmail.com>
2018-01-03 10:18:26 +0100

    feat: add deposit case to reducer

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -4,6 +4,8 @@ export default function balanceReducer(state = 0, action) {
   switch (action.type) {
     case constants.SET_BALANCE:
       return action.balance;
+    case constants.DEPOSIT:
+      return action.deposit + state;
     default:
       return state;
   }

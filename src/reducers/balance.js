19a06158a52eafed598d0efc6dd99456ea089741
joseph_ctu <sephxd1234@gmail.com>
2018-01-03 10:26:43 +0100

    feat: add withdraw case to reducer

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -6,6 +6,8 @@ export default function balanceReducer(state = 0, action) {
       return action.balance;
     case constants.DEPOSIT:
       return action.deposit + state;
+    case constants.WITHDRAW:
+      return state - action.withdraw;
     default:
       return state;
   }

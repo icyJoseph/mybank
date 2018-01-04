4ce404b294a65f46cb553178671c6c5595c41db4
joseph_ctu <sephxd1234@gmail.com>
2018-01-04 10:47:19 +0100

    test: action dispatch when Loot mounts

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import { Loot } from "./Loot";
 
 describe("Loot", () => {
-  const loot = shallow(<Loot />);
+  const mockfetchBitcoin = jest.fn();
+  const props = { balance: 10, bitcoin: {} };
+  let loot = shallow(<Loot {...props} />);
 
   it("renders properly", () => {
     expect(loot).toMatchSnapshot();
+  });
+
+  describe("when mounted", () => {
+    beforeEach(() => {
+      props.fetchBitcoin = mockfetchBitcoin;
+      loot = mount(<Loot {...props} />);
+    });
+
+    it("dispatched the fetchBitcoint() it receives from props", () => {
+      expect(mockfetchBitcoin).toHaveBeenCalled();
+    });
   });
 });

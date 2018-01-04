e36441e74e19a4e2b9a0830a7589452823c1c9c2
joseph_ctu <sephxd1234@gmail.com>
2018-01-04 11:13:47 +0100

    test: display of bitcoin balance is correct

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -4,7 +4,7 @@ import { Loot } from "./Loot";
 
 describe("Loot", () => {
   const mockfetchBitcoin = jest.fn();
-  const props = { balance: 10, bitcoin: {} };
+  let props = { balance: 10, bitcoin: {} };
   let loot = shallow(<Loot {...props} />);
 
   it("renders properly", () => {
@@ -14,11 +14,23 @@ describe("Loot", () => {
   describe("when mounted", () => {
     beforeEach(() => {
       props.fetchBitcoin = mockfetchBitcoin;
+      // By mounting the Loot component, componentDidMount() is triggered
       loot = mount(<Loot {...props} />);
     });
 
     it("dispatched the fetchBitcoint() it receives from props", () => {
       expect(mockfetchBitcoin).toHaveBeenCalled();
+    });
+  });
+
+  describe("when there are valid bitcoin props", () => {
+    beforeEach(() => {
+      props = { balance: 10, bitcoin: { bpi: { USD: { rate: "1,000" } } } };
+      loot = shallow(<Loot {...props} />);
+    });
+
+    it("displays the correct bitcoin value", () => {
+      expect(loot.find("h3").text()).toEqual("Bitcoin balance: 0.01");
     });
   });
 });

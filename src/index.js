614cf412025538a3b735ff1d84d3b9a9078e84a2
joseph_ctu <sephxd1234@gmail.com>
2018-01-04 10:31:41 +0100

    feat: apply middleware thunk to store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 
 import App from "./components/App";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>

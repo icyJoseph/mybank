530ca64ebb917243ab4a29831f701c6f6441308d
joseph_ctu <sephxd1234@gmail.com>
2018-01-04 10:37:33 +0100

    feat: implement Loot component

---
+++ src/components/Loot.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class Loot extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Powered By CoinDesk</h3>
+        <h3>Bitcoin balance:</h3>
+      </div>
+    );
+  }
+}
+
+export default Loot;

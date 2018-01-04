85fea4f66744dc38c4410666af54800127e8022d
joseph_ctu <sephxd1234@gmail.com>
2018-01-04 10:22:40 +0100

    refactor: import rootReducer correctly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./reducers/balance";
+import rootReducer from "./reducers";
 
 import App from "./components/App";
 

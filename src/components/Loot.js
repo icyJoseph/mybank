1d09c5a7ed1d8f4bc4a183257878fc10f54a402b
joseph_ctu <sephxd1234@gmail.com>
2018-01-04 10:49:27 +0100

    fix: bitcoin contains the bitcoint value in the bpi key

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -3,12 +3,16 @@ import { connect } from "react-redux";
 import { fetchBitcoin } from "../actions/bitcoin";
 
 export class Loot extends Component {
+  componentDidMount() {
+    this.props.fetchBitcoin();
+  }
+
   render() {
     const { bitcoin } = this.props;
     return (
       <div>
         <h3>Powered By CoinDesk</h3>
-        <h3>Bitcoin balance: {bitcoin}</h3>
+        <h3>Bitcoin balance: {bitcoin.bpi}</h3>
       </div>
     );
   }

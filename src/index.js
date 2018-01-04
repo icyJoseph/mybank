1607d0e363a2c9fe467c273062bc17ee88e7d1fd
joseph_ctu <sephxd1234@gmail.com>
2018-01-04 12:01:50 +0100

    style: basic style added to the page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 
 import App from "./components/App";
+import "./index.css";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
